fa5469c2090700323bb6299e772de32d40887663
RiviereGregory <gregory.riviere@laposte.net>
2021-02-07 14:49:52 +0100

    Jeu addition ajout score

--- jeu_de_math/main.py
+++ jeu_de_math/main.py
@@ -2,10 +2,13 @@ import random
 
 NOMBRE_MIN = 1
 NOMBRE_MAX = 10
+NB_QUESTION = 10
 
 
 def poser_question():
+    global nombre_de_reponse
     nombre_int = 0
+    nb_faux = 0
     gagne = False
     nombre1 = random.randint(NOMBRE_MIN, NOMBRE_MAX)
     nombre2 = random.randint(NOMBRE_MIN, NOMBRE_MAX)
@@ -16,13 +19,23 @@ def poser_question():
         except:
             print("ERREUR: vous devez rentrer un nombre")
         else:
+            nombre_de_reponse += 1
             if (nombre1 + nombre2) == nombre_int:
                 gagne = True
                 print("Réponse correcte")
             else:
                 print("Réponse incorrecte")
+                nb_faux += 1
+    return 0 if not nb_faux == 0 else 1
 
 
-for i in range(0, 10):
-    poser_question()
+nombre_de_reponse = 0
+nb_de_point = 0
+for i in range(0, NB_QUESTION):
     i += 1
+    print(f"Question {i}/{NB_QUESTION} :")
+    nb_de_point += poser_question()
+    print()
+
+print(f"{nombre_de_reponse} propositions pour {NB_QUESTION} questions")
+print(f"Scores: {nb_de_point} / {NB_QUESTION} ")
