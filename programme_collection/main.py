c84b842b51b1dccb50f1a1114b9483008b7c5246
RiviereGregory <gregory.riviere@laposte.net>
2021-02-10 19:04:53 +0100

    Utilisation liste

--- programme_collection/main.py
+++ programme_collection/main.py
@@ -24,5 +24,26 @@ for i in personnes:
     print(len(i))
     print(i[-1])
 
+# Liste
+personnesListe = ["Mélanie", "Jean", "Martin", "Alice"]
+print(personnesListe)
+print()
+
+# Ajout dans liste
+nouvelle_persone = "David"
+personnesListe.append(nouvelle_persone)
+print(personnesListe)
+print()
+
+# suppression dans la liste
+del personnesListe[1]
+print(personnesListe)
+print()
 
 
+def afficher_personnes(c):
+    for i in c:
+        print(i)
+
+
+afficher_personnes(personnesListe)
