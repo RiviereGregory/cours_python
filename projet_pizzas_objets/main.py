40f7875ce6414c9304fea7ff41356ed5644eac4c
RiviereGregory <gregory.riviere@laposte.net>
2021-02-14 16:27:58 +0100

    ajout pizza personalisée

--- projet_pizzas_objets/main.py
+++ projet_pizzas_objets/main.py
@@ -17,10 +17,32 @@ class Pizza:
         print()
 
 
+class PizzaPersonalisee(Pizza):
+    PRIX_DE_BASE = 7
+    PRIX_PAR_INGREDIENT = 1.2
+
+    def __init__(self):
+        super().__init__("Personnalisée", 0, [])
+        self.demander_ingredients_utilisateur()
+        self.caluler_prix()
+
+    def demander_ingredients_utilisateur(self):
+        while True:
+            ingredient = input("Ajoutez un ingrédient (ou ENTER pour terminer) :")
+            if ingredient == "":
+                return
+            self.ingredients.append(ingredient)
+            print(f"""vous avez {len(self.ingredients)} ingrédient(s) : {", ".join(self.ingredients)}""")
+
+    def caluler_prix(self):
+        self.prix = PizzaPersonalisee.PRIX_DE_BASE + (len(self.ingredients) * PizzaPersonalisee.PRIX_PAR_INGREDIENT)
+
+
 pizzas = [Pizza("4 fromages", 8.5, ("brie", "emmental", "comté", "parmesan", "sauce tomate"), True),
           Pizza("Jambon", 9, ("jambon", "emmental", "crème")),
           Pizza("Corsica", 10.5, ("brucciu", "emmental", "figatéli", "sauce tomate")),
-          Pizza("mozzarella", 8.5, ("mozarella", "crème"), True)]
+          Pizza("mozzarella", 8.5, ("mozarella", "crème"), True),
+          PizzaPersonalisee()]
 
 
 def tri(e):
