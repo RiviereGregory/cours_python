4cd140408f42933d7e2247bebd6933b7a12f9a8d
RiviereGregory <gregory.riviere@laposte.net>
2021-02-14 16:10:50 +0100

    exercie ajout pizza végétarienne et ajout affichage divers

--- projet_pizzas_objets/main.py
+++ projet_pizzas_objets/main.py
@@ -1,21 +1,40 @@
 # nom, prix, ingredients, végétarienne
 
 class Pizza:
-    def __init__(self, nom, prix, ingredients):
+    def __init__(self, nom, prix, ingredients, vegetarienne=False):
         self.nom = nom
         self.prix = prix
         self.ingredients = ingredients
+        self.vegetarienne = vegetarienne
 
     def afficher(self):
-        print(f"PIZZA {self.nom} : {self.prix}€")
+        veg_str = ""
+        if self.vegetarienne:
+            veg_str = " - VEGETARIENNE"
+
+        print(f"PIZZA {self.nom} : {self.prix}€" + veg_str)
         print(", ".join(self.ingredients))
         print()
 
 
-pizzas = (Pizza("4 fromages", 8.5, ("brie", "emmental", "comté", "parmesan")),
+pizzas = (Pizza("4 fromages", 8.5, ("brie", "emmental", "comté", "parmesan", "sauce tomate"), True),
           Pizza("Jambon", 9, ("jambon", "emmental", "crème")),
           Pizza("Corsica", 10.5, ("brucciu", "emmental", "figatéli", "sauce tomate")),
-          Pizza("mozzarella", 8.5, ("mozarella", "crème")))
+          Pizza("mozzarella", 8.5, ("mozarella", "crème"), True))
 
+print("--- Pizza végétarienne ---")
 for pizza in pizzas:
-    pizza.afficher()
+    if pizza.vegetarienne:
+        pizza.afficher()
+print("--- Pizza non végétarienne ---")
+for pizza in pizzas:
+    if not pizza.vegetarienne:
+        pizza.afficher()
+print("--- Pizza sauce tomate ---")
+for pizza in pizzas:
+    if "sauce tomate" in pizza.ingredients:
+        pizza.afficher()
+print("--- Pizza moins de 10€ ---")
+for pizza in pizzas:
+    if pizza.prix < 10:
+        pizza.afficher()
