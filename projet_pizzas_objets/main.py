3822dac1e2c7c9da1b715ba919f6f7b4a9365b97
RiviereGregory <gregory.riviere@laposte.net>
2021-02-14 15:52:28 +0100

    Ajout ingrédients

--- projet_pizzas_objets/main.py
+++ projet_pizzas_objets/main.py
@@ -1,13 +1,16 @@
 # nom, prix, ingredients, végétarienne
 
 class Pizza:
-    def __init__(self, nom, prix):
+    def __init__(self, nom, prix, ingredients):
         self.nom = nom
         self.prix = prix
+        self.ingredients = ingredients
 
     def afficher(self):
         print(f"PIZZA {self.nom} : {self.prix}€")
+        print(", ".join(self.ingredients))
+        print()
 
 
-pizza1 = Pizza("4 fromages", 8.5)
+pizza1 = Pizza("4 fromages", 8.5, ("brie", "emmental", "comté", "parmesan"))
 pizza1.afficher()
