63a95280cc412991465371cf7259034d9e019a19
RiviereGregory <gregory.riviere@laposte.net>
2021-02-28 11:19:30 +0100

    Ajout exemple utilisation dictionnaire

--- programme_dictionnaire/main.py
+++ programme_dictionnaire/main.py
@@ -13,3 +13,40 @@ print(personne)
 
 for i in personne:
     print("clef : " + i)
+
+# --- PARTIE 2---
+
+# noms age,taille
+
+personnes = [
+    ("Mélanie", 25, 1.6),
+    ("Paul", 29, 1.8),
+    ("Jacques", 35, 1.75),
+    ("Martin", 16, 1.65)
+]
+
+
+def obtenir_information(nom, liste):
+    for i in liste:
+        if i[0] == nom:
+            return i
+    return None
+
+
+# Jacques
+infos = obtenir_information("Jacques", personnes)
+print(infos)
+
+# Avec dictionnaire
+personnes_dict = {
+    "Mélanie": (25, 1.6),
+    "Paul": (29, 1.8),
+    "Jacques": (35, 1.75),
+    "Martin": (16, 1.65)
+}
+
+# acces direct à l'info sans boucle
+infos_dic = personnes_dict.get("Jacques")
+print(infos_dic)
+infos_dic = personnes_dict.get("Claire")
+print(infos_dic)
