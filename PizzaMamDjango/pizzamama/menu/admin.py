8469bbc9e458e8a730dbae1ae670c29c79a19c62
RiviereGregory <gregory.riviere@laposte.net>
2021-03-30 20:11:25 +0200

    Amélioration de l'interface et ajout de Pizza

--- PizzaMamDjango/pizzamama/menu/admin.py
+++ PizzaMamDjango/pizzamama/menu/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Pizza
 
+
+# permet d'afficher les colonnes dans l'interface
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'ingredients', 'vegetarienne', 'prix')
+    search_fields = ['nom']
+
+
 # Register your models here.
-admin.site.register(Pizza)
+admin.site.register(Pizza, PizzaAdmin)
