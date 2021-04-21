924c61fc3ebbd5077ec70b8d3d33e0670137e8bb
RiviereGregory <gregory.riviere@laposte.net>
2021-04-21 19:58:53 +0200

    [django] Ajout API rest pour récuperer les pizzas

--- PizzaMamDjango/pizzamama/menu/views.py
+++ PizzaMamDjango/pizzamama/menu/views.py
@@ -1,3 +1,5 @@
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Pizza
@@ -8,3 +10,9 @@ from .models import Pizza
 def index(request):
     pizzas = Pizza.objects.all().order_by('prix')
     return render(request, 'menu/index.html', {'pizzas': pizzas})
+
+
+def api_get_pizzas(request):
+    pizzas = Pizza.objects.all().order_by('prix')
+    json = serializers.serialize("json", pizzas)
+    return HttpResponse(json)
