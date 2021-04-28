8e5b3707b95762bebf3bbeafe426599120a0aa70
RiviereGregory <gregory.riviere@laposte.net>
2021-04-28 17:59:15 +0200

    [KIVY] Ajout BoxLayout en code python

--- kivy-lelab/main.py
+++ kivy-lelab/main.py
@@ -1,6 +1,8 @@
 import os
 
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
 # Utile pour le pb d'openGL 1.1 pas 2.0
@@ -9,6 +11,18 @@ os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 class MainWidget(Widget):
     pass
+
+
+class BoxLayoutExemple(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = "vertical"
+        b1 = Button(text="A")
+        b2 = Button(text="B")
+        b3 = Button(text="C")
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3)
 
 
 class LelabApp(App):
