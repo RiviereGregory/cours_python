6f9f3ce391f7a12eef75d597d25d48f5ab09c7b2
RiviereGregory <gregory.riviere@laposte.net>
2021-04-28 18:01:51 +0200

    [KIVY] Ajout BoxLayout équivalent en text kv

--- kivy-lelab/main.py
+++ kivy-lelab/main.py
@@ -14,15 +14,16 @@ class MainWidget(Widget):
 
 
 class BoxLayoutExemple(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "vertical"
-        b1 = Button(text="A")
-        b2 = Button(text="B")
-        b3 = Button(text="C")
-        self.add_widget(b1)
-        self.add_widget(b2)
-        self.add_widget(b3)
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     self.orientation = "vertical"
+    #     b1 = Button(text="A")
+    #     b2 = Button(text="B")
+    #     b3 = Button(text="C")
+    #     self.add_widget(b1)
+    #     self.add_widget(b2)
+    #     self.add_widget(b3)
+    pass
 
 
 class LelabApp(App):
