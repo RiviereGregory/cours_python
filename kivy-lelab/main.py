905b65fa5281b6c82a399a22615e006681fded8f
RiviereGregory <gregory.riviere@laposte.net>
2021-04-29 20:45:54 +0200

    [KIVY] Ajout utilisation du click du bouton

--- kivy-lelab/main.py
+++ kivy-lelab/main.py
@@ -2,14 +2,24 @@ import os
 
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 # Utile pour le pb d'openGL 1.1 pas 2.0
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+
+
+class WidgetsExample(GridLayout):
+    mon_texte = StringProperty("Bonjour!")
+
+    def on_button_click(self):
+        print("Button click")
+        self.mon_texte = "hello"
 
 
 class MainWidget(Widget):
