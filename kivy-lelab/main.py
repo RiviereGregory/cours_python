02755bd4a4bc21e1144628d938cc22ced1d97673
RiviereGregory <gregory.riviere@laposte.net>
2021-04-29 20:21:44 +0200

    [KIVY] Ajout utilisation de StackLayout

--- kivy-lelab/main.py
+++ kivy-lelab/main.py
@@ -1,8 +1,11 @@
 import os
 
 from kivy.app import App
+from kivy.metrics import dp
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 # Utile pour le pb d'openGL 1.1 pas 2.0
@@ -11,6 +14,14 @@ os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 class MainWidget(Widget):
     pass
+
+
+class StackLayoutExemple(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(0, 25):
+            b = Button(text=str(i + 1), size_hint=(None, None), size=(dp(100), dp(100)))
+            self.add_widget(b)
 
 
 # class GridLayoutExemple(GridLayout):
