927a599a44e2baebf54b9bfee3e8523c0fa35774
RiviereGregory <gregory.riviere@laposte.net>
2021-05-03 10:39:11 +0200

    [KIVY] navigation ajout d'un boxlayout pour l'ActionBar pour éviter de dupliquer le code

--- kivy-lelab/main.py
+++ kivy-lelab/main.py
@@ -1,13 +1,24 @@
 import os
 
 from kivy.app import App
-
 # Utile pour le pb d'openGL 1.1 pas 2.0
+from kivy.properties import ObjectProperty
+
+from navigation_screen_manager import NavigationScreenManager
+
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 
 
-class LelabApp(App):
+class MyScreenManager(NavigationScreenManager):
     pass
+
+
+class LelabApp(App):
+    manager = ObjectProperty(None)
+
+    def build(self):
+        self.manager = MyScreenManager()
+        return self.manager
 
 
 LelabApp().run()
