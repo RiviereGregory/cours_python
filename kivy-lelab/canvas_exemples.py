c791d1227c3492807cf62d0ecf5b3941a4cfa4cf
RiviereGregory <gregory.riviere@laposte.net>
2021-05-06 16:40:09 +0200

    [KIVY] canvas deplacer le rectangle

--- kivy-lelab/canvas_exemples.py
+++ kivy-lelab/canvas_exemples.py
@@ -1,5 +1,6 @@
 from kivy.graphics import Line, Color, Rectangle
 from kivy.lang import Builder
+from kivy.metrics import dp
 from kivy.uix.widget import Widget
 
 # permet de charger un fichier kv
@@ -26,4 +27,9 @@ class CanvasExemple4(Widget):
             Color(0, 1, 0)
             Line(circle=(400, 200, 80), width=2)
             Line(rectangle=(100, 400, 150, 100), width=2)
-            Rectangle(pos=(100, 200), size=(150, 100))
+            self.rect = Rectangle(pos=(100, 200), size=(150, 100))
+
+    def on_button_a_click(self):
+        x, y = self.rect.pos
+        x += dp(10)
+        self.rect.pos = (x, y)
