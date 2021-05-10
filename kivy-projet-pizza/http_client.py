6b06824fa82d2fb1ed5d9e2bac443faa5e2d2486
RiviereGregory <gregory.riviere@laposte.net>
2021-05-10 11:53:44 +0200

    [PIZZA][KIVY] gestion des erreurs avec api

--- kivy-projet-pizza/http_client.py
+++ kivy-projet-pizza/http_client.py
@@ -4,7 +4,7 @@ from kivy.network.urlrequest import UrlRequest
 
 
 class HttpClient:
-    def get_pizzas(self, on_complete):
+    def get_pizzas(self, on_complete, on_error):
         url = "https://jrpizzamamadjangogri.herokuapp.com/api/GetPizzas"
 
         def data_received(req, result):
@@ -14,4 +14,14 @@ class HttpClient:
             if on_complete:
                 on_complete(pizza_dict)
 
-        req = UrlRequest(url, on_success=data_received)
+        def data_error(req, error):
+            print("data_error")
+            if on_error:
+                on_error(error.args[0])
+
+        def data_failure(req, result):
+            print("data_failure")
+            if on_error:
+                on_error("Erreur serveur : " + str(req.resp_status))
+
+        req = UrlRequest(url, on_success=data_received, on_error=data_error, on_failure=data_failure)
--- kivy-projet-pizza/main.py
+++ kivy-projet-pizza/main.py
@@ -19,14 +19,18 @@ class PizzaWidget(BoxLayout):
 
 class MainWidget(FloatLayout):
     recycleView = ObjectProperty(None)
+    error_str = StringProperty("")
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        HttpClient.get_pizzas(self, self.on_server_data)
+        HttpClient.get_pizzas(self, self.on_server_data, self.on_server_error)
 
     def on_server_data(self, pizzas_dict):
         self.recycleView.data = pizzas_dict
+
+    def on_server_error(self, error):
+        self.error_str = "ERROR: " + error
+        print(self.error_str)
 
 
 with open("pizzascr.kv", encoding='utf8') as f:
