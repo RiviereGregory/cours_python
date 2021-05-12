0b96b0b7a371c947b97b141e544cb3479e3d93b6
RiviereGregory <gregory.riviere@laposte.net>
2021-05-12 20:15:05 +0200

    [Galaxy] Ajout ligne verticales

--- galaxy/main.py
+++ galaxy/main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.graphics import Color, Line
 from kivy.properties import *
 from kivy.uix.widget import Widget
 
@@ -7,26 +8,44 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
+    V_NB_LINES = 7
+    V_LINES_SPACING = 0.1  # pourcentage sur la largeru de l'écran
+    vertical_lines = []
+
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
-        print("INIT W: " + str(self.width) + " H: " + str(self.height))
+        self.init_vertical_lines()
 
     def on_parent(self, widget, parent):
-        print("ON PARENT W: " + str(self.width) + " H: " + str(self.height))
+        pass
 
     def on_size(self, *args):
-        # print("ON SIZE W: " + str(self.width) + " H: " + str(self.height))
-        # Si on ne les met pas dans le fichier kv on les utiliser ici
-        # pour les initialiser
-        # self.perspective_point_x = self.width / 2
-        # self.perspective_point_y = self.height * 0.75
+        self.update_vertical_lines()
         pass
 
     def on_perspective_point_x(self, widget, value):
-        print("PX: " + str(value))
+        pass
 
     def on_perspective_point_y(self, widget, value):
-        print("PY: " + str(value))
+        pass
+
+    def init_vertical_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_lines.append(Line())
+
+    def update_vertical_lines(self):
+        centrale_line_x = self.width / 2
+        spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NB_LINES / 2)
+        for i in range(0, self.V_NB_LINES):
+            x1 = int(centrale_line_x + offset * spacing)
+            y1 = 0
+            x2 = x1
+            y2 = self.height
+            self.vertical_lines[i].points = [x1, y1, x2, y2]
+            offset += 1
 
 
 class GalaxyApp(App):
