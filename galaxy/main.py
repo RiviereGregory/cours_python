e5745becf05d95d424e5adf047f8139ad18b0d17
RiviereGregory <gregory.riviere@laposte.net>
2021-05-15 15:15:54 +0200

    [Galaxy] Ajout lignes horizontale

--- galaxy/main.py
+++ galaxy/main.py
@@ -9,25 +9,50 @@ class MainWidget(Widget):
     perspective_point_y = NumericProperty(0)
 
     V_NB_LINES = 10
-    V_LINES_SPACING = 0.2  # pourcentage sur la largeru de l'écran
+    V_LINES_SPACING = 0.1  # pourcentage sur la largeur de l'écran
     vertical_lines = []
+
+    H_NB_LINES = 15
+    H_LINES_SPACING = 0.2  # pourcentage sur la hauteur de l'écran
+    horizontal_lines = []
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.init_vertical_lines()
+        self.init_horizontal_lines()
 
     def on_parent(self, widget, parent):
         pass
 
     def on_size(self, *args):
         self.update_vertical_lines()
-        pass
+        self.update_horizontal_lines()
 
     def on_perspective_point_x(self, widget, value):
         pass
 
     def on_perspective_point_y(self, widget, value):
         pass
+
+    def init_horizontal_lines(self):
+        with self.canvas:
+            Color(1, 1, 1)
+            for i in range(0, self.H_NB_LINES):
+                self.horizontal_lines.append(Line())
+
+    def update_horizontal_lines(self):
+        centrale_line_x = self.width / 2
+        spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NB_LINES / 2) + 0.5
+
+        xmin = centrale_line_x + offset * spacing
+        xmax = centrale_line_x - offset * spacing
+        spacing_y = self.H_LINES_SPACING * self.height
+        for i in range(0, self.H_NB_LINES):
+            line_y = i * spacing_y
+            x1, y1 = self.transform(xmin, line_y)
+            x2, y2 = self.transform(xmax, line_y)
+            self.horizontal_lines[i].points = [x1, y1, x2, y2]
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -40,7 +65,7 @@ class MainWidget(Widget):
         spacing = self.V_LINES_SPACING * self.width
         offset = -int(self.V_NB_LINES / 2) + 0.5
         for i in range(0, self.V_NB_LINES):
-            line_x = int(centrale_line_x + offset * spacing)
+            line_x = centrale_line_x + offset * spacing
             x1, y1 = self.transform(line_x, 0)
             x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
@@ -58,14 +83,14 @@ class MainWidget(Widget):
 
         tr_x = self.perspective_point_x + offset_x
 
-        return tr_x, tr_y
+        return int(tr_x), int(tr_y)
 
     def transform(self, pt_x, pt_y):
         # return self.transform_2d(pt_x, pt_y)
         return self.transform_perspective(pt_x, pt_y)
 
     def transform_2d(self, pt_x, pt_y):
-        return pt_x, pt_y
+        return int(pt_x), int(pt_y)
 
 
 class GalaxyApp(App):
