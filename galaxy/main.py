e5d01b3bd1f1d36e02af18f81e6a7b82eb3e558a
RiviereGregory <gregory.riviere@laposte.net>
2021-05-15 15:36:48 +0200

    [Galaxy] Ajout mouvement terrain

--- galaxy/main.py
+++ galaxy/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.graphics import Color, Line
 from kivy.properties import *
+from kivy.properties import Clock
 from kivy.uix.widget import Widget
 
 
@@ -16,17 +17,20 @@ class MainWidget(Widget):
     H_LINES_SPACING = 0.1  # pourcentage sur la hauteur de l'écran
     horizontal_lines = []
 
+    SPEED = 4
+    current_offset_y = 0
+
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.init_vertical_lines()
         self.init_horizontal_lines()
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
 
     def on_parent(self, widget, parent):
         pass
 
     def on_size(self, *args):
-        self.update_vertical_lines()
-        self.update_horizontal_lines()
+        pass
 
     def on_perspective_point_x(self, widget, value):
         pass
@@ -49,7 +53,7 @@ class MainWidget(Widget):
         xmax = centrale_line_x - offset * spacing
         spacing_y = self.H_LINES_SPACING * self.height
         for i in range(0, self.H_NB_LINES):
-            line_y = i * spacing_y
+            line_y = i * spacing_y - self.current_offset_y
             x1, y1 = self.transform(xmin, line_y)
             x2, y2 = self.transform(xmax, line_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
@@ -93,6 +97,15 @@ class MainWidget(Widget):
 
     def transform_2d(self, pt_x, pt_y):
         return int(pt_x), int(pt_y)
+
+    def update(self, dt):
+        self.update_vertical_lines()
+        self.update_horizontal_lines()
+        self.current_offset_y += self.SPEED
+
+        spacing_y = self.H_LINES_SPACING * self.height
+        if self.current_offset_y >= spacing_y:
+            self.current_offset_y -= spacing_y
 
 
 class GalaxyApp(App):
