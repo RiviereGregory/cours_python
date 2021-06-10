51d8fd8c82721bc49efb2550f7209f84be835d78
RiviereGregory <gregory.riviere@laposte.net>
2021-06-10 20:59:14 +0200

    [MrBeat] Ajout play indicator

--- mrbeat/main.py
+++ mrbeat/main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
+from kivy.metrics import dp
+from kivy.properties import ObjectProperty, NumericProperty
 from kivy.uix.relativelayout import RelativeLayout
 
 from audio_engine import AudioEngine
@@ -8,12 +9,15 @@ from sounds_kit_service import SoundsKitService
 from track import TrackWidget
 
 Builder.load_file("track.kv")
+Builder.load_file("play_indicator.kv")
 
 TRACK_NB_STEP = 16
 
 
 class MainWidget(RelativeLayout):
     tracks_layout = ObjectProperty()
+    play_indicator_widget = ObjectProperty()
+    TRACK_STEPS_LEFT_ALIGN = NumericProperty(dp(100))
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -29,9 +33,12 @@ class MainWidget(RelativeLayout):
                                                     TRACK_NB_STEP)
 
     def on_parent(self, widget, parent):
+        self.play_indicator_widget.set_nb_steps(TRACK_NB_STEP)
+        self.play_indicator_widget.set_current_step_index(10)
         for i in range(0, self.sound_kit_service.get_nb_tracks()):
             sound = self.sound_kit_service.get_sound_at(i)
-            self.tracks_layout.add_widget(TrackWidget(sound, self.audio_engine, TRACK_NB_STEP, self.mixer.tracks[i]))
+            self.tracks_layout.add_widget(
+                TrackWidget(sound, self.audio_engine, TRACK_NB_STEP, self.mixer.tracks[i], self.TRACK_STEPS_LEFT_ALIGN))
 
 
 class MrBeatApp(App):
